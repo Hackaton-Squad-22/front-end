3e317f8213abc230072051cac0a827466de84f31
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-11-04 14:08:29 -0300

    Lógica de iteração entre os conteúdos do front

--- src/controller/cursosController.js
+++ src/controller/cursosController.js
@@ -2,13 +2,15 @@ import adicionaCurso from "../service/adicionaCurso.js";
 import enviaCursos from "../service/enviaCursos.js";
 import pesquisaCursos from "../service/pesquisaCursos.js";
 
-const conteudo = document.querySelector(".conteudo");
+const conteudos = document.querySelectorAll(".conteudo");
 const response = pesquisaCursos("http://localhost:4000/users");
 const cursosIniciados = response[1] || [];
 const userId = response[0];
 
-conteudo.addEventListener("click", () => {
-  const cursosId = adicionaCurso(cursosIniciados);
-
-  enviaCursos(`http://localhost:4000/users/${userId}`, cursosId);
-});
+for (let conteudo of conteudos) {
+  conteudo.addEventListener("click", () => {
+    const cursosId = adicionaCurso(cursosIniciados);
+  
+    enviaCursos(`http://localhost:4000/users/${userId}`, cursosId);
+  });
+}
