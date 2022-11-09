cd325be1429ee3e8045e4f718be0d615ea22cd2a
Marcelo Lemes <marceloaflemes@gmail.com>
2022-11-09 19:02:07 -0300

    feat: criando barra de progresso circular

---
+++ script.js
@@ -0,0 +1,109 @@
+const urlUsers = 'https://squad22-hackathon.herokuapp.com/users';
+const urlFullstack = 'https://squad22-hackathon.herokuapp.com/fullstacks';
+const urlUX = 'https://squad22-hackathon.herokuapp.com/ux';
+const urlQA = 'https://squad22-hackathon.herokuapp.com/qa';
+
+// selectPlan('Fullstacks');
+// selectPlan('UX');
+selectPlan('QA');
+
+function selectPlan(name){
+    switch(name){
+        case 'QA':
+            getUserAndCourses(urlQA, name);
+            break;
+        case 'UX':
+            getUserAndCourses(urlUX, name);
+            break;
+        default:
+            getUserAndCourses(urlFullstack, name);
+    }
+}
+
+async function getUserAndCourses(url, name){
+    try{
+        const userRes = await fetch(urlUsers);
+        const users = userRes.json();
+
+        const courseRes = await fetch(url);
+        const courses = await courseRes.json();
+        Promise.all([users, courses]).then(values => compareCourses(values, name));
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+function getUser(promiseValue){
+    const users = promiseValue[0];
+    const currentId = getCurrentUserId();
+    const currentUser = users.find(user => user._id === currentId);
+    return currentUser;
+}
+
+function getCurrentUserId(){
+    const preIdLength = location.pathname.indexOf('/', 1);
+    const idStart = preIdLength + 1;
+    const currentId = location.pathname.substring(idStart);
+    return currentId;
+}
+
+function compareCourses(promiseValue, name){
+    const user = getUser(promiseValue)
+    const courses = promiseValue[1];
+    let filteredCourses = [];
+    if (name === 'Fullstacks'){
+        filteredCourses = getCoursesIdList(courses).filter(course => {
+            switch(name){
+                case 'QA':
+                    return user.cursosQa.includes(course);
+                case 'UX':
+                    return user.Ux.includes(course);
+                default:
+                    return user.cursosFullstacks.includes(course);
+            }
+        })
+    }
+    return findPercentage(courses.length, filteredCourses.length);
+}
+
+function getCoursesIdList(courses){
+    const coursesId = courses.map(course => {
+        return course._id;
+    });
+    return coursesId;
+}
+
+function findPercentage(total, amount){
+    let percentage = 0;
+    if (amount > 0){
+        percentage = ((amount / total)*100).toFixed(2);
+        console.log('porcentagem', percentage)
+        showProgressBar(percentage);
+        return;
+    }
+    showProgressBar(0);
+}
+
+function showProgressBar(percentage){
+    const progressBar = document.querySelector('[data-progress="circle"]');
+    const progressNumber = document.querySelector('[data-progress="number-circcle"]');
+
+    const userProgressString = `${percentage}%`;
+    console.log(userProgressString);
+
+    const progressDeg = `${(percentage * 3.6) + 90}deg`;
+    
+    if(percentage > 0 && percentage <= 50){
+        progressBar.style.background = `
+            linear-gradient(to right, #CCC 50%, transparent 50%, transparent),
+            linear-gradient(${progressDeg}, blue 50%, #CCC 50%)
+        `;
+    }
+    if(percentage > 50){
+        progressBar.style.background = `
+        linear-gradient(to left, blue 50%, transparent 50%, transparent),
+        linear-gradient(${progressDeg}, blue 50%, #CCC 50%)
+        `;
+    }    
+    progressNumber.textContent = `${userProgress}%`;
+}
