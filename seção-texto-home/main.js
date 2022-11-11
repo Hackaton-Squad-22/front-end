1be86b8c43449333ec91b19ac2c93fde74f4bd58
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-11-11 20:34:35 -0300

    Fix: Responsividade

--- seção-texto-home/main.js
+++ seção-texto-home/main.js
@@ -1,16 +1,18 @@
-const quebraLinhas = document.querySelectorAll(".br")
-const secaoMedia = document.querySelector(".secao-media-home")
+const quebraLinhas = document.querySelectorAll(".br");
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("resize", () => {
   if (window.innerWidth <= 744 && window.innerWidth > 430) {
     for (let linha of quebraLinhas) {
-      linha.classList.toggle("table-txt")
+      console.log(linha);
+      linha.classList.remove("desabilitado");
     }
   }
 
-/*   console.log(secaoMedia);
-  console.log(window.innerWidth);
-  if ( window.innerWidth <= 430 ) {
-    secaoMedia.classList.add("text-center")
-  } */
-})
+  if (window.innerWidth > 744) {
+    for (let linha of quebraLinhas) {
+      console.log(linha);
+      linha.classList.add("desabilitado");
+    }
+  }
+
+});
