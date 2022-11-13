6b20c3dc6a2c97b580129750b9184ac552790452
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-11-13 00:42:19 -0300

    Editando modal e adicionando opcçoes de preenchimento

--- src/service/cursos/editaCurso.js
+++ src/service/cursos/editaCurso.js
@@ -2,6 +2,7 @@ const editaCurso = () => {
   const modal = document.querySelector("#modal");
   const abrirModal = document.querySelector(".adiciona-curso");
   const fecharModal = document.querySelector(".fechar-modal");
+  const enviarDados = document.querySelector(".btn-adicionar");
 
   abrirModal.addEventListener("click", () => {
     modal.showModal();
@@ -10,6 +11,12 @@ const editaCurso = () => {
   fecharModal.addEventListener("click", () => {
     modal.close();
   });
+
+  enviarDados.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log("teste");
+  })
+
 };
 
 export default editaCurso;
