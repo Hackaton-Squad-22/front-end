fc849e7352a825c8912f40a28b2d68826ffc8212
Marcelo Lemes <marceloaflemes@gmail.com>
2022-11-14 16:25:55 -0300

    Incluindo script e data attributes das barras de progresso das páginas de trilhas

--- src/service/conteudosUser/exibeProgresso.js
+++ src/service/conteudosUser/exibeProgresso.js
@@ -4,7 +4,9 @@ export function exibeProgresso(porcentagem, trilha){
   const barraDeProgresso = selecionaBarraDeProgresso(trilha);
   const numeroDoProgresso = selecionaNumeroDoProgresso(trilha);
 
-  const progressoDoUsuarioString = `${porcentagem}%`;
-  barraDeProgresso.style.transform = `translateX(${progressoDoUsuarioString})`;
-  numeroDoProgresso.textContent = progressoDoUsuarioString;
+  if(barraDeProgresso && numeroDoProgresso){
+    const progressoDoUsuarioString = `${porcentagem}%`;
+    barraDeProgresso.style.transform = `translateX(${progressoDoUsuarioString})`;
+    numeroDoProgresso.textContent = progressoDoUsuarioString;
+  }
 };
