c89679bf5b8c2e9424816cc65c0492eaac241d44
Marcelo Lemes <marceloaflemes@gmail.com>
2022-11-14 18:49:14 -0300

    Substituindo id dinâmico (pathname) pelo id fixo de demonstração

--- src/service/conteudosUser/obtemUsuario.js
+++ src/service/conteudosUser/obtemUsuario.js
@@ -9,5 +9,11 @@ function obtemIdUsuarioAtual(){
   const preIdLength = location.pathname.indexOf("/", 1);
   const inicioId = preIdLength + 1;
   const idAtual = location.pathname.substring(inicioId);
-  return idAtual;
+  // Id provisório (utilizado apenas para demostração da funcionalidade)
+  return '636ba55ae03538b208450ee6';
+  
+  // Caso o href da aplicação seja no formato abaixo, com o pathname '/users/{id}', retirar o retorno dom o id provisório e substituir pelo idAtual
+  // 'https://endereco.com/users/0123456789'
+
+  return idAtual; //inativo
 };
