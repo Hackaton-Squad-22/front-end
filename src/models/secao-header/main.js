54192d7322bf2528c5fd1bdaff6390aaf03c171c
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-11-14 22:07:27 -0300

    Reparo no header

--- src/models/secao-header/main.js
+++ src/models/secao-header/main.js
@@ -1,7 +1,13 @@
 const p = document.querySelector('.texto-comunidade');
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth <= 744) {
+  if (window.innerWidth <= 1000) {
     p.innerHTML = 'comunidade'
+  }
+})
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1000) {
+    p.innerHTML = 'junte-se a comunidade'
   }
 })
