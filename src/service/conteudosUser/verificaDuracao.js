c01b718df8cac89ec31e6ec5210765bc280bae47
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2022-11-14 21:15:40 -0300

    Display de horas

--- src/service/conteudosUser/verificaDuracao.js
+++ src/service/conteudosUser/verificaDuracao.js
@@ -1,10 +1,41 @@
-const verificaDuracao = (duracao) => {
+const displayHoras = (duracao) => {
+  if (duracao == "") {
+    return ""
+  } 
+
+  let final = ""
+  let horaFinal = ""
+  let minutofinal = ""
+
   const filtro = duracao.split(":")
-  if (filtro[0] == "00") {
-    return `${filtro[1]}:${filtro[2]}`
+  if (filtro[0] != "00") {
+    if (filtro[0][0] != "0") {
+      horaFinal = filtro[0]
+    } else {
+      horaFinal = filtro[0][1]
+    }
+    if (filtro[1] != "00") {
+      if (filtro[1][0] != "0") {
+        minutofinal = filtro[1]
+      } else {
+        minutofinal = filtro[1][1]
+      }
+    }
+    final = `${horaFinal}h${minutofinal}m`
   } else {
-    return duracao
+    if (filtro[1] != "00") {
+      if (filtro[1][0] != "0") {
+        minutofinal = filtro[1]
+      } else {
+        minutofinal = filtro[1][1]
+      }
+      final = `${minutofinal}m`
+    } else {
+      final = ""
+    }
   }
+
+return final
 }
 
-export default verificaDuracao
+export default displayHoras
